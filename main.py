cd5a9a96181daad0823bce816a6b10000029db9d
Anthony Kritsas <ankrits@teiemt.gr>
2019-04-09 04:54:46 +0300

    Refactoring some code

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import numpy as np
 import cv2
 
 
-
 # Initiate the Cameras Feed
 camera = cv2.VideoCapture(0)
 
 while True:
     # Reading the camera frames
     _, frame = camera.read()
+
     # Blurring the frame
     blurred_frame = cv2.GaussianBlur(frame, (5, 5), 0)
     hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
@@ -22,21 +22,14 @@ while True:
     _, contours = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     ret, thresh = cv2.threshold(hsv, 127, 255, 0)
 
-    for contour in contours:
-        df = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        cv2.drawContours(df, mask, 1, (0, 255, 0), 3)
-
-
     # Calling to display the two pictures in order to find the countoures
     cv2.imshow("Frame", frame)
     cv2.imshow("Mask", mask)
 
     # Defining the Exit Key
-    exitKey = cv2.waitKey(0)
+    exitKey = cv2.waitKey(5000)
     if exitKey == 27:
         break
-
 
 camera.release()
 cv2.destroyAllWindows()
