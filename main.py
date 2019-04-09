7cbc421bce901e0337c82e09926b5f30dd215633
Anthony Kritsas <ankrits@teiemt.gr>
2019-04-09 05:48:55 +0300

    building the basic rig of the algorithm

--- main.py
+++ main.py
@@ -1,35 +1,67 @@
 # Created By Antonios Kritsas
-# Importing Opencv & Numpy
 import numpy as np
 import cv2
 
 
-# Initiate the Cameras Feed
-camera = cv2.VideoCapture(0)
+def getRectangle():
+    # Importing the image into the function
+    img = cv2.imread(file_name)
 
-while True:
-    # Reading the camera frames
-    _, frame = camera.read()
+    # Clone the image
+    img_final = cv2.imread(file_name)
 
-    # Blurring the frame
-    blurred_frame = cv2.GaussianBlur(frame, (5, 5), 0)
-    hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
-    lower_blue = np.array([38, 86, 0])
-    upper_blue = np.array([121, 255, 255])
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    # Making the image gray scale
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Finding the contours
-    _, contours = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    ret, thresh = cv2.threshold(hsv, 127, 255, 0)
+    # Adding threshold to the image by setting it to binary
+    ret, mask = cv2.threshold(img_gray, 180, 255, cv2.THRESH_BINARY)
 
-    # Calling to display the two pictures in order to find the countoures
-    cv2.imshow("Frame", frame)
-    cv2.imshow("Mask", mask)
+    image_final = cv2.bitwise_and(img_gray, img_gray, mask=mask)
+    # Adding threshold for black text
+    ret, new_img = cv2.threshold(image_final, 180, 255, cv2.THRESH_BINARY)
 
-    # Defining the Exit Key
-    exitKey = cv2.waitKey(5000)
-    if exitKey == 27:
-        break
+    # Calculating the orientation of the squares
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+    dilated = cv2.dilate(new_img, kernel, iterations=9)
 
-camera.release()
+    # Getting Contours
+    contours, hierachy = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    index = 0
+    for contour in contours:
+
+        # Get rectangle bounding contour
+        [x, y, w, h] = cv2.boundingRect(contour)
+
+        if w < 35 and h < 35:
+            continue
+
+            # draw rectangle around contour on original image
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
+        # cv2.resizeWindow('Image', 600, 600)
+        cropped = img_final[y:y + h, x: x + w]
+
+        s = file_name + '/' + str(index) + '.jpg'
+        cv2.imwrite(s, cropped)
+        index = index + 1
+
+
+        # If the listener catches the esc key close all the windows
+        key = cv2.waitKey()
+        if key == 27:
+            break
+
+
+
+
+
+
+
+
+
+
+
 cv2.destroyAllWindows()
+# Importing the 22.PNG as variable
+file_name = 'cards/22.png'
+# Starting the sequence
+getRectangle()
